66c2d410a86f5ef0b422fd4231d85957665ce49d
Codewithharsh09 <wardhanh886@gmail.com>
2025-01-23 15:58:32 +0530

    Create server

---
+++ src/app.js
@@ -0,0 +1,17 @@
+    const express = require("express");
+    const app = express();
+    // const port =7000;
+    app.use("/test",(req,res)=>{
+        res.send("Namaste from the server")
+    })
+    app.use("/test2",(req,res)=>{
+        res.send("RAM RAM from the server")
+    })
+    app.use("/hello",(req,res)=>{
+        res.send("HELLO HELLO HELLO")
+    }) 
+    app.listen(7000,()=>{
+        console.log("server is successfully listening on port 3000")
+    });
+    console.log("NEW CHANGES");
+    

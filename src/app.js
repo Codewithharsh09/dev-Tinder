ae5c3ca5374fad033b2ae221b6a26a3de3830936
Codewithharsh09 <wardhanh886@gmail.com>
2025-01-25 20:47:31 +0530

    error-handling

--- src/app.js
+++ src/app.js
@@ -2,26 +2,21 @@ const express = require("express");
 const app = express();
 // const port =7000;
 
-const {adminAuth,userAuth} = require("./middleware/auth");
-//Handle Auth Middleware for all request GET,POST
-app.use("/admin",adminAuth);
-// app.use("/user",userAuth);
+app.use("/", (err, req, res, next) => {
+    if (err) {
+        res.status(500).send("Something went wrong")
+    }
+});
+app.get("/getUserData", (req, res) => {
+    try {
+        //Logic of db call anf geta user data
+        throw new Error("hjzdzfh")
+        res.send("User Data Sent")
+    } catch (err) {
+        res.status(500).send("Some error contact support team")
+    }
+});
 
-app.post("user/login",(req,res)=>{
-    res.send("User logged in succefffully")
-});
-app.get("/user/data", userAuth,(req, res) => {
-    res.send("User Data Sent");
-});
-app.get("/newUser",userAuth,(req,res)=>{
-    res.send({firstName:"Harsh",lastName : "Wardhan"})
-});
-    app.get("/admin/getAllData", (req, res) => {
-        res.send("All Data Sent");
-});
-app.get("/admin/deleteUser", (req, res) => {
-    res.send("Deleted User");
-});
 app.listen(7000, () => {
     console.log("server is successfully listening on port 7000")
 });

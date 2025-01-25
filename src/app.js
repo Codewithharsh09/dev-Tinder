d4a07b44abb97164550293537f55e59f6d00376c
Codewithharsh09 <wardhanh886@gmail.com>
2025-01-25 11:12:35 +0530

    route handling

--- src/app.js
+++ src/app.js
@@ -1,17 +1,31 @@
     const express = require("express");
     const app = express();
     // const port =7000;
-    app.use("/test",(req,res)=>{
-        res.send("Namaste from the server")
-    })
-    app.use("/test2",(req,res)=>{
-        res.send("RAM RAM from the server")
-    })
-    app.use("/hello",(req,res)=>{
-        res.send("HELLO HELLO HELLO")
-    }) 
-    app.listen(7000,()=>{
-        console.log("server is successfully listening on port 3000")
+    
+    app.get("/user/:userId/:password/:key",
+    (req,res,next)=>{
+        console.log("Handling the route user !!")
+        res.send(req.params)
+        console.log(req.params)
+        next()
+    },
+    (req,res,next)=>{
+        console.log("Handling the route user 2!!")
+        // res.send("2nd Response !!");
+        next()
+    },
+    (req,res,next)=>{
+        console.log("Handling the route user 3!!")
+        // res.send("3rd Response !!");
+        next();
+
+    },
+    (req,res,next)=>{
+        console.log("Handling the route user 4!!")
+        // res.send("4th Response !!")
+        next();
     });
-    console.log("NEW CHANGES");
-    console.log("1234567890")
+
+    app.listen(7000,()=>{
+        console.log("server is successfully listening on port 7000")
+    });

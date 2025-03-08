29462c0f0f10dc8ca830d96ed693c28aacc58b2c
Codewithharsh09 <wardhanh886@gmail.com>
2025-03-09 01:57:56 +0530

    routing

---
+++ homework.md
@@ -0,0 +1,28 @@
+Ep-03 | Creating our Express Server:-
+- Create a repository
+- Initialize the repository
+- node_modules, Package.json, package-lock.json
+- Install express
+- Create a Server
+- Listen to port 7777
+- Write request handlers for /test , /hello
+- Install nodemon and update scripts inside package.json
+- What are dependencies and Devdependencies 
+- difference between dependencies and devdependencies
+- what is the use of "-g" while npm install 
+- difference betw
+
+Ep-04 | Routing and request Handlers:-
+
+- initialize git
+- .gitignore
+- Create a remote repo on github
+- push all code to remote origin
+- play with routes and route extensions ex. /hello , /hello/2 , /xyz
+- order of the routes matter a lot
+- Install postman app and make a workspace/collection > test API call
+- Write logic to handle GET,POST,PATCH,DELETE API Calls and test then on Postman
+- Explore routing and use of ?,+,(),* in the routes
+- Use of regex in routes /a/, /.*fly$/
+- Routing the query params in the routes
+- routing the dynamic routes
--- src/app.js
+++ src/app.js
@@ -1,131 +1,151 @@
-const express = require("express");
-const connectDB = require("./config/database");
-const app = express();
-// const port =7000;
-const User = require("./models/user");
-app.use(express.json())
-//POST API ==========>>>>>>
-app.post("/signup",async (req,res)=>{
-    // console.log(req.body)
-  //Creating a new instance of the user model -->
-    const user = new User(req.body);
-    try{
-        await user.save();
-        res.send("User added successfully !!");
-    }catch(err){
-        // console.log("ERROR",err )
-        res.status(400).send(err)
-    }
-});
-// app.post("/newUserr", async (req,res)=>{
-//     const user = new User({
-//         firstName : "Pradhuman",
-//         lastName : "Singh",
-//         emailId : "pradh12@gmail.com",
-//         age : 24
-//     })
+// const express = require("express");
+// const connectDB = require("./config/database");
+// const app = express();
+// // const port =7000;
+// const User = require("./models/user");
+// app.use(express.json())
+// //POST API ==========>>>>>>
+// app.post("/signup",async (req,res)=>{
+//     // console.log(req.body)
+//   //Creating a new instance of the user model -->
+//     const user = new User(req.body);
 //     try{
-//         await user.save()
-//         res.send("User added Successfully !!")
+//         await user.save();
+//         res.send("User added successfully !!");
 //     }catch(err){
-//         console.log(err)
+//         // console.log("ERROR",err )
+//         res.status(400).send(err)
 //     }
 // });
+// // app.post("/newUserr", async (req,res)=>{
+// //     const user = new User({
+// //         firstName : "Pradhuman",
+// //         lastName : "Singh",
+// //         emailId : "pradh12@gmail.com",
+// //         age : 24
+// //     })
+// //     try{
+// //         await user.save()
+// //         res.send("User added Successfully !!")
+// //     }catch(err){
+// //         console.log(err)
+// //     }
+// // });
 
-// app.patch("/newUserr",async (req,res)=>{
-//     const userId = req.body.userId;
-//     const data = req.body
-//     try{
-//         await User.findByIdAndUpdate({_id : userId},data);
-//         res.send("user updated successfully !!")
-//     }catch(err){
-//         console.log("Something went wrong !!")
+// // app.patch("/newUserr",async (req,res)=>{
+// //     const userId = req.body.userId;
+// //     const data = req.body
+// //     try{
+// //         await User.findByIdAndUpdate({_id : userId},data);
+// //         res.send("user updated successfully !!")
+// //     }catch(err){
+// //         console.log("Something went wrong !!")
+// //     }
+// // })
+// //Get User by email--------->>
+// app.get("/user",async (req,res)=>{
+//     const userEmail = req.body.emailId;
+//    try{
+//     const users = await User.findOne({emailId : userEmail});
+//     if(users.length === 0){
+//         res.status(404).send("User not Found")
 //     }
+//    res.send(users)
+//    }catch(err){
+//     res.status(400).send("Something went wrong")
+//    }
 // })
-//Get User by email--------->>
-app.get("/user",async (req,res)=>{
-    const userEmail = req.body.emailId;
-   try{
-    const users = await User.findOne({emailId : userEmail});
-    if(users.length === 0){
-        res.status(404).send("User not Found")
-    }
-   res.send(users)
-   }catch(err){
-    res.status(400).send("Something went wrong")
-   }
-})
 
-//Feed API - GET/Feed - get all the user from the database ----->>>>
-app.get("/feed",async (req,res)=>{
-    const userEmail = req.body.emailId;
-    try{
-     const users = await User.find({});
-     if(!users){
-         res.status(404).send("User not Found")
-     }
-    res.send(users)
-    }catch(err){
-     res.status(400).send("Something went wrong")
-    }    
-
-});
-
-//Delete API-------->>>>>>
-app.delete("/user",async (req,res)=>{
-    const userId = req.body.userId;
-    try{
-        const users = await User.findByIdAndDelete(userId);
-        // const users = await User.findByIdAndDelete({_id : userId});
-        if(!users){
-            res.status(404).send("User not Found")
-        }
-       res.send("User deleted successfully !")
-       }catch(err){
-        res.status(400).send("Something went wrong")
-       }    
-   
-});
-
-//Update data to the user ---------->>>>>>>
-
-app.patch("/user", async(req,res)=>{
-const userId = req.body.userId;
-const data = req.body;
-try{
-const user = await User.findByIdAndUpdate({_id : userId},data,{
-    returnDocument:"after",
-    runValidators:true,
-})
-console.log(user)
-res.send("User updated successfully !!")
-}catch(err){
-    res.status(404).send("UPDATE FAILED :" +err)
-}
-})
-
-
-// app.delete("/newUser", async (req,res)=>{
-//     const userEmail = req.body.emailId
+// //Feed API - GET/Feed - get all the user from the database ----->>>>
+// app.get("/feed",async (req,res)=>{
+//     const userEmail = req.body.emailId;
 //     try{
-//         await User.deleteOne({emailId : userEmail});
-//         res.send("User deleted Successfully !!")
+//      const users = await User.find({});
+//      if(!users){
+//          res.status(404).send("User not Found")
+//      }
+//     res.send(users)
 //     }catch(err){
-//         res.status(404).send(err)
-//     }
+//      res.status(400).send("Something went wrong")
+//     }    
+
 // });
 
+// //Delete API-------->>>>>>
+// app.delete("/user",async (req,res)=>{
+//     const userId = req.body.userId;
+//     try{
+//         const users = await User.findByIdAndDelete(userId);
+//         // const users = await User.findByIdAndDelete({_id : userId});
+//         if(!users){
+//             res.status(404).send("User not Found")
+//         }
+//        res.send("User deleted successfully !")
+//        }catch(err){
+//         res.status(400).send("Something went wrong")
+//        }    
+   
+// });
 
-connectDB()
-.then(()=>{
-    console.log("Databse connected successfully");
-    app.listen(7000, () => {
-        console.log("server is successfully listening on port 7000");
-    });
-}).catch(()=>{
-    console.log("Database cannot connected")
-});
+// //Update data to the user ---------->>>>>>>
+
+// app.patch("/user/:userId", async(req,res)=>{
+// const userId = req.params?.userId;
+// const data = req.body;
+
+// try{
+//     const ALLOWED_UPDATES = [
+//         "userId","photoUrl","about","gender","age","skills"
+//     ];
+//     const isUpdateAllowed = Object.keys(data).every(k=>ALLOWED_UPDATES.includes(k));
+//     if(!isUpdateAllowed){
+//         throw new Error("Update not allowed");
+//     }
+//     if(data?.skills.length>10){
+//         throw new Error("Skills can not be more than 10")
+//     }
+// const user = await User.findByIdAndUpdate({_id : userId},data,{
+//     returnDocument:"after",
+//     runValidators:true,
+// })
+// console.log(user)
+// res.send("User updated successfully !!")
+// }catch(err){
+//     res.status(404).send("UPDATE FAILED :" +err)
+// }
+// })
 
 
+// // app.delete("/newUser", async (req,res)=>{
+// //     const userEmail = req.body.emailId
+// //     try{
+// //         await User.deleteOne({emailId : userEmail});
+// //         res.send("User deleted Successfully !!")
+// //     }catch(err){
+// //         res.status(404).send(err)
+// //     }
+// // });
 
 
+// connectDB()
+// .then(()=>{
+//     console.log("Databse connected successfully");
+//     app.listen(7000, () => {
+//         console.log("server is successfully listening on port 7000");
+//     });
+// }).catch(()=>{
+//     console.log("Database cannot connected")
+// });
+
+const express = require('express');
+const app = express()
+
+//this will only handle GET call to /user
+app.get("/ab?c",(req,res)=>{
+res.send({firstName :"Harshwardhan",lastName:"Singh"})
+})
+
+
+app.listen(3000,()=>{
+    console.log("Server is successfully listening on port 3000")
+})
